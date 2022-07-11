e32cda44974f9497103e553b661a7d993d1c36b5
David Windell <david@outeredgeuk.com>
2022-07-11 15:55:16 +0000

    Add PHPCS

--- registration.php
+++ registration.php
@@ -1,7 +1,9 @@
 <?php
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'OuterEdge_Multiplenewsletter',
-    __DIR__
-);
+if (class_exists(\Magento\Framework\Component\ComponentRegistrar::class)) {
+    \Magento\Framework\Component\ComponentRegistrar::register(
+        \Magento\Framework\Component\ComponentRegistrar::MODULE,
+        'OuterEdge_Multiplenewsletter',
+        __DIR__
+    );
+}
